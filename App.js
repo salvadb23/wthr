a570622738549f918605880e732fdd7bc6c75907
Salvador <salbecerra1023@gmail.com>
2019-04-16 13:35:03 -0700

    correct weather now displaying

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import DisplayWeather from './Components/DisplayWeather'
+import DisplayWeather from './components/DisplayWeather'
+import { WEATHER_API_KEY } from 'react-native-dotenv'
+
 
 export default class App extends React.Component {
+
+  state = {
+    location: null,
+    weather: null
+  }
+
+  loadWeather() {
+    const apikey = WEATHER_API_KEY
+    const { latitude, longitude } = this.state.location.coords
+    const units = 'Imperial'
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=${units}`
+    // `fetch()` is used to load data
+    console.log(url)
+    fetch(url)
+    // After a connection is made the data is streamed as JSON
+    .then(res => res.json())
+    // When that resolves we use `this.setState()` to assign the weather data to state which will also re-render the component.
+    .then(json => this.setState({ weather: json }))
+    .catch(err => console.log(err.message))
+
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      console.log(location)
+      this.setState({ location })
+      this.loadWeather()
+      console.log(this.state)
+    }, (err) => {
+      console.log(err.message)
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <DisplayWeather />
+        <DisplayWeather data={this.state.weather} />
       </View>
     );
   }
--- components/DisplayWeather.js
+++ components/DisplayWeather.js
@@ -2,12 +2,24 @@ import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
 const DisplayWeather = (props) => {
-  return (
-        <View style={styles.container}>
-            <Text style={styles.temp}>72˚</Text>
-        </View>
-  )
-}
+    // No data yet, so it must still be loading
+    if (props.data === null) {
+      return <Text>Loading...</Text>
+    }
+    // Cod is where codes go, so if an error occurred, the value would not be 200 (could be 400, 404, etc.)
+    if (props.data.cod !== 200) {
+      return <Text>An Error has occurred</Text>
+    }
+    // If no error and not loading, load/display weather data, grabbing the appropriate data from props
+    const { temp } = props.data.main
+    const { description } = props.data.weather[0]
+    return (
+      <View style={styles.container}>
+        <Text style={styles.temp}>{temp}˚</Text>
+        <Text>{description}</Text>
+      </View>
+    )
+  }
 
 const styles = StyleSheet.create({
     container: {
